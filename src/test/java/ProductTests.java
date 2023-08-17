1cfef6dc7bec1e0dbccad6b245c11a2204e24424
vladdmutriv <vladdmutriv@mail.com>
2023-08-17 10:28:08 +0300

    push

---
+++ src/test/java/ProductTests.java
@@ -0,0 +1,14 @@
+import io.restassured.response.Response;
+import org.junit.Test;
+import starter.stepdefinitions.CarsAPI;
+import starter.stepdefinitions.SearchStepDefinitions;
+
+public class ProductTests {
+
+    @Test
+    public void test(){
+        Response response = new SearchStepDefinitions().heCallsEndpoint("//waarkoop-server.herokuapp.com/api/v1/search/demo/apple");
+        String string;
+
+    }
+}
